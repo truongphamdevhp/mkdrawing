51abd1ffc3ddc5574285980d43fdf3d4235140f2
Xuan Truong Pham <truongphamhp92@gmail.com>
2022-03-11 20:55:05 +0700

    quên mất ko add file

---
+++ lib/mkheader.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const double dDefaultThickness = 50;
+const double dMinSliderVal = 5;
+const double dMaxSlideVal = 50;
+const Color clPenDefault = Color.fromARGB(255, 79, 55, 184);
+const Color clBackgroundDefault = Color.fromARGB(255, 255, 255, 255);
